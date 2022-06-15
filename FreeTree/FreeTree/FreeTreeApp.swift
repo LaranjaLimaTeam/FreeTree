6db5f65407616699f4b938b13c8aff4f228fcbf0
Giordano Mattiello <giordanomattiello@MacBook-Pro-de-Giordano.local>
2022-06-15 10:18:21 -0300

    initial commit

---
+++ FreeTree/FreeTree/FreeTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTreeApp.swift
+//  FreeTree
+//
+//  Created by Giordano Mattiello on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreeTree/FreeTree/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FreeTree
+//
+//  Created by Giordano Mattiello on 15/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
