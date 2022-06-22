dd10e22276cde0b40cc08e1f52eaa3dd0ee1c92a
Pedro Mota <p.smota98@gmail.com>
2022-06-22 10:48:06 -0300

    Protocol and implementation of create Tree method

--- FreeTree/FreeTree/Managers/JsonManager.swift
+++ FreeTree/FreeTree/Managers/JsonManager.swift
@@ -15,28 +15,40 @@ struct JsonManager {
         return nil
     }
 
-    static public func saveJson<T: Encodable>(data: T) {
+    static public func saveJson<T: Codable>(data: T) -> T? {
+        var dataArray: [T]? = decoding(fileName: "myJsonData")
+        
+        if var dataArray = dataArray {
+            dataArray.append(contentsOf: [data])
+        } else {
+            dataArray = [data]
+        }
+        
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         do {
-            let codableJSON = try encoder.encode(data)
+            let codableJSON = try encoder.encode(dataArray)
             let fileManager = FileManager.default
             let directoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
             let fileURL = directoryURL.appendingPathComponent("JSON").appendingPathComponent("myJsonData.json")
             do {
                 try codableJSON.write(to: fileURL, options: [.atomic])
+                return data
             } catch {
                 print("Error while saving to path. Error: \(error)")
                 do {
                     try fileManager.createDirectory(at: directoryURL.appendingPathComponent("JSON"),
                                                     withIntermediateDirectories: false)
                     try codableJSON.write(to: fileURL, options: [.atomic])
+                    return data
                 } catch {
                     print("Error when creating file, Error: \(error)")
+                    return nil
                 }
             }
         } catch {
             print("Failed to encode, error: \(error)")
+            return nil
         }
     }
 }
--- FreeTree/FreeTree/Managers/TreeManager.swift
+++ FreeTree/FreeTree/Managers/TreeManager.swift
@@ -2,11 +2,15 @@
 //  TreeManager.swift
 //  FreeTree
 //
-//  Created by Pedro Mota on 21/06/22.
+//  Created by Pedro Mota on 22/06/22.
 //
 
 import Foundation
 
 protocol TreeManager {
-    func create(_ tree: Tree, completion: @escaping (Result<Tree, Error>) -> Void)
+    func create(_ tree: Tree, completion: @escaping (Result<Tree, TreeManagerError>) -> Void)
+}
+
+enum TreeManagerError: Error {
+    case creationError
 }
