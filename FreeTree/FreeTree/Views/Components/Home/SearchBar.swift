2682b62a3777d51cc6c4984dbb0d0eb2401d3f9f
nathan-batista <70816956+nathan-batista@users.noreply.github.com>
2022-06-22 10:59:59 -0300

    Ft 05/search bar (#5)

---
+++ FreeTree/FreeTree/Views/Components/Home/SearchBar.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+    @Binding var searching: Bool
+    let searchAction: () -> Void
+    let cleanData: () -> Void
+    let placeHolderText: String
+    var body: some View {
+        HStack {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color.init(uiColor: UIColor.lightGray))
+                    .opacity(0.4)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(Color.gray)
+                        .padding(.leading, 8)
+                    TextField(placeHolderText, text: $searchText).onSubmit {
+                        searching = true
+                        searchAction()
+                    }
+                    Spacer()
+                    Button {
+                        searching = false
+                        cleanData()
+                        searchText = ""
+                    } label: {
+                        Image(systemName: "x.circle.fill")
+                            .foregroundColor(Color.gray)
+                            .padding(.trailing, 13)
+                    }
+                }
+            }
+            .cornerRadius(8)
+            .padding(.leading, 16)
+        }.frame(height: 38)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(
+            searchText: .constant(""),
+            searching: .constant(true),
+            searchAction: {return},
+            cleanData: {return},
+            placeHolderText: "Pesquisar"
+        )
+    }
+}
