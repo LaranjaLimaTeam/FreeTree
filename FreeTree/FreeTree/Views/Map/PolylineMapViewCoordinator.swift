b4d03f8903abcac8cd137f5aa536b49c110d5348
Giordano Mattiello <giordanomattiello@MacBook-Pro-de-Giordano.local>
2022-07-05 11:08:04 -0300

    fix click on self location error

--- FreeTree/FreeTree/Views/Map/PolylineMapViewCoordinator.swift
+++ FreeTree/FreeTree/Views/Map/PolylineMapViewCoordinator.swift
@@ -13,6 +13,7 @@ final class PolylineMapViewCoordinator: NSObject, MKMapViewDelegate {
         self.map = control
     }
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if view.reuseIdentifier != "tree" { return }
         let treeArray = map.mapViewModel.treesOnMap
         let tree = treeArray.filter {
             if let subtitle = view.annotation?.subtitle {
