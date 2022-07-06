08df83607d1e2b1676386d4241b174350416d7b1
Giordano Mattiello <giordanomattiello@MacBook-Pro-de-Giordano.local>
2022-07-06 09:19:50 -0300

    fix select bug

--- FreeTree/FreeTree/Views/Map/PolylineMapViewCoordinator.swift
+++ FreeTree/FreeTree/Views/Map/PolylineMapViewCoordinator.swift
@@ -13,6 +13,7 @@ final class PolylineMapViewCoordinator: NSObject, MKMapViewDelegate {
         self.map = control
     }
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation, animated: false)
         if view.reuseIdentifier != "tree" { return }
         let treeArray = map.mapViewModel.treesOnMap
         let tree = treeArray.filter {
