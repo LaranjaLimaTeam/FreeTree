c34d875c4115a0df7b089b42473f335e5f72f7db
Pedro Mota <p.smota98@gmail.com>
2022-07-12 11:19:18 -0300

    fixed folder pbxproj issue

---
+++ FreeTree/FreeTree/Views/AddTreeView/AddTreeForm.swift
@@ -0,0 +1,68 @@
+//
+//  AddTreeForm.swift
+//  FreeTree
+//
+//  Created by Pedro Mota on 12/07/22.
+//
+
+import SwiftUI
+
+struct AddTreeForm: View {
+    
+    private var isPresented: Binding<Bool>
+    private var isCapturingPhoto: Binding<Bool>
+    @ObservedObject var addTreeViewModel: AddTreeViewModel
+    
+    init(_ isPresented: Binding<Bool>, _ isCapturingPhoto: Binding<Bool>, addTreeViewModel: AddTreeViewModel) {
+        self.isPresented = isPresented
+        self.isCapturingPhoto = isCapturingPhoto
+        self.addTreeViewModel = addTreeViewModel
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            TextField(
+                "User name (email address)",
+                text: $addTreeViewModel.tree.name
+            )
+            .padding(.horizontal, 16)
+            .frame(height: 44)
+            .background(.white)
+            Section(title: "Imagens") {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        CapturePhotoButton(isCapturingPhoto: self.isCapturingPhoto)
+                        ForEach(Array(addTreeViewModel.photos.enumerated()), id: \.0) { _, image in
+                            ImageView(for: image)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 12)
+            ToggleField("Foi plantada por você?", value: .constant(true))
+            ToggleField("É frutífera?", value: .constant(true))
+            Section(title: "Endereço") {
+                Group {
+                    Text("Praça Henfil")
+                    Text("Cidade Universitária")
+                    Text("Campinas - SP")
+                    Text("13083")
+                    Text("Brasil")
+                        .padding(.bottom, 12)
+                }
+            }
+            .padding(.horizontal, 16)
+            LargeButton(title: "Adicionar árvore") {
+                withAnimation {
+                    addTreeViewModel.addTree()
+                    self.isPresented.wrappedValue.toggle()
+                }
+            }
+            Spacer()
+        }
+        .padding(.top, 16)
+        .frame(maxHeight: .infinity)
+        .background(Color.init(uiColor: .systemGray5))
+    }
+}
---
+++ FreeTree/FreeTree/Views/AddTreeView/AddTreeView.swift
@@ -0,0 +1,40 @@
+//
+//  AddTreeView.swift
+//  FreeTree
+//
+//  Created by Pedro Mota on 22/06/22.
+//
+
+import SwiftUI
+
+struct AddTreeView: View {
+    
+    @State var isCapturingPhoto: Bool = false
+    @State var capturedImage: UIImage? = nil
+    @Binding var isPresented: Bool
+    @ObservedObject var addTreeViewModel = AddTreeViewModel()
+    
+    var body: some View {
+        if isCapturingPhoto {
+            CaptureImageView(
+                isShown: $isCapturingPhoto,
+                image: $capturedImage,
+                images: $addTreeViewModel.photos
+            )
+        } else {
+            AddTreeForm(
+                $isPresented,
+                $isCapturingPhoto,
+                addTreeViewModel: addTreeViewModel
+            )
+        }
+    }
+}
+
+struct AddTreeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTreeView(
+            isPresented: .constant(true)
+        )
+    }
+}
